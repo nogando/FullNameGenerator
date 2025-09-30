e3922b0e8f0e8a29b1b0ac495ea66a9d18d8a38e
nogando <nathanielogando@gmail.com>
2025-09-30 17:26:39 -0400

    name Generator push 1

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,49 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        //first name
+        System.out.println("Enter your First name");
+        String firstName = scanner.nextLine();
+
+
+        //Middle name
+        System.out.println("Enter your Middle Initial or Middle Name");
+        String middleName = scanner.nextLine();
+
+        //Last name
+        System.out.println("Enter your Last name");
+        String lastName = scanner.nextLine();
+
+
+        //Suffix
+        System.out.println("Suffix:");
+        String suffix = scanner.nextLine();
+
+        // variables
+
+        if (middleName.isEmpty() && suffix.isEmpty()){
+            System.out.printf("First name: %s \nLast name: %s\n\nFull Name:\n%s %s\n",firstName,lastName,firstName,lastName);
+
         }
+        else if (middleName.isEmpty() && !suffix.isEmpty()){
+
+            System.out.printf("First name: %s \nLast name: %s \nSuffix: %s\n\nFull Name:\n%s %s, %s\n",firstName,lastName,suffix,firstName,lastName,suffix);
+
+        }
+        else if(suffix.isEmpty() && !middleName.isEmpty()){
+            System.out.printf("First name: %s \nMiddle name: %s \nLast Name: %s\n\nFull Name:\n%s %s. %s\n",firstName,middleName,lastName,firstName,middleName,lastName);
+        }
+        else{
+            System.out.printf("First name: %s \nMiddle name: %s \nLast Name: %s \nSuffix: %s\n\nFull Name:\n%s %s. %s, %s\n",firstName,middleName,lastName,suffix,firstName,middleName,lastName,suffix);
+        }
+
+
     }
 }
